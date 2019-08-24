510ae795b7cd85c920bf65c6e7792bb4e4024130
Jacky Feng <jackyfeng530@yahoo.com>
2019-08-24 18:23:23 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   array.lenth.times do |counter|
     if array[counter] == value_to_find
       return counter
-      
+    end 
 end
 
 def find_max_value(array)
